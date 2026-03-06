90ca0ba3f24d149496e2b0fdf6f8300f927bd5a8
randomuser234-g <thoratkihome@gmail.com>
2026-03-06 13:16:28 -0600

    fix the invisible barriers to use the new mappings
    now it looks proper again

--- _maps/Invisible Barriers.asm
+++ _maps/Invisible Barriers.asm
@@ -1,30 +1,27 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - invisible solid blocks
-; ---------------------------------------------------------------------------
-Map_Invis_internal:	mappingsTable
-	mappingsTableEntry.w	.solid
-	mappingsTableEntry.w	.unused1
-	mappingsTableEntry.w	.unused2
+Map_7a16: mappingsTable
+	mappingsTableEntry.w	Map_7a16_0
+	mappingsTableEntry.w	Map_7a16_1
+	mappingsTableEntry.w	Map_7a16_2
 
-.solid:	spriteHeader
-	spritePiece	-$10, -$10, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	0, -$10, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	-$10, 0, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	0, 0, 2, 2, $18, 0, 0, 0, 0
-.solid_End
+Map_7a16_0:	spriteHeader
+ spritePiece -$10, -$10, 2, 2, $15, 0, 0, 0, 0
+ spritePiece 0, -$10, 2, 2, $15, 0, 0, 0, 0
+ spritePiece -$10, 0, 2, 2, $15, 0, 0, 0, 0
+ spritePiece 0, 0, 2, 2, $15, 0, 0, 0, 0
+Map_7a16_0_End
 
-.unused1:	spriteHeader
-	spritePiece	-$40, -$20, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	$30, -$20, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	-$40, $10, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	$30, $10, 2, 2, $18, 0, 0, 0, 0
-.unused1_End
+Map_7a16_1:	spriteHeader
+ spritePiece -$40, -$20, 2, 2, $15, 0, 0, 0, 0
+ spritePiece $30, -$20, 2, 2, $15, 0, 0, 0, 0
+ spritePiece -$40, $10, 2, 2, $15, 0, 0, 0, 0
+ spritePiece $30, $10, 2, 2, $15, 0, 0, 0, 0
+Map_7a16_1_End
 
-.unused2:	spriteHeader
-	spritePiece	-$80, -$20, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	$70, -$20, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	-$80, $10, 2, 2, $18, 0, 0, 0, 0
-	spritePiece	$70, $10, 2, 2, $18, 0, 0, 0, 0
-.unused2_End
+Map_7a16_2:	spriteHeader
+ spritePiece -$80, -$20, 2, 2, $15, 0, 0, 0, 0
+ spritePiece -$80, $10, 2, 2, $15, 0, 0, 0, 0
+ spritePiece $70, -$20, 2, 2, $15, 0, 0, 0, 0
+ spritePiece $70, $10, 2, 2, $15, 0, 0, 0, 0
+Map_7a16_2_End
 
 	even
