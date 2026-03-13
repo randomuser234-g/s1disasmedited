72c8950d53244296a7835210456c38c0cb5b0e5d
randomuser234-g <thoratkihome@gmail.com>
2026-03-13 16:37:19 -0500

    knuckles monitor p2
    mappings for it after Tails

--- _maps/Monitor.asm
+++ _maps/Monitor.asm
@@ -1,103 +1,111 @@
-Map_e82f: mappingsTable
-	mappingsTableEntry.w	Map_e82f_0
-	mappingsTableEntry.w	Map_e82f_1
-	mappingsTableEntry.w	Map_e82f_2
-	mappingsTableEntry.w	Map_e82f_3
-	mappingsTableEntry.w	Map_e82f_4
-	mappingsTableEntry.w	Map_e82f_5
-	mappingsTableEntry.w	Map_e82f_6
-	mappingsTableEntry.w	Map_e82f_7
-	mappingsTableEntry.w	Map_e82f_8
-	mappingsTableEntry.w	Map_e82f_9
-	mappingsTableEntry.w	Map_e82f_10
-	mappingsTableEntry.w	Map_e82f_11
-	mappingsTableEntry.w	Map_e82f_12
+Map_b1fd: mappingsTable
+	mappingsTableEntry.w	Map_b1fd_0
+	mappingsTableEntry.w	Map_b1fd_1
+	mappingsTableEntry.w	Map_b1fd_2
+	mappingsTableEntry.w	Map_b1fd_3
+	mappingsTableEntry.w	Map_b1fd_4
+	mappingsTableEntry.w	Map_b1fd_5
+	mappingsTableEntry.w	Map_b1fd_6
+	mappingsTableEntry.w	Map_b1fd_7
+	mappingsTableEntry.w	Map_b1fd_8
+	mappingsTableEntry.w	Map_b1fd_9
+	mappingsTableEntry.w	Map_b1fd_10
+	mappingsTableEntry.w	Map_b1fd_11
+	mappingsTableEntry.w	Map_b1fd_12
+	mappingsTableEntry.w	Map_b1fd_13
 
-Map_e82f_0:	spriteHeader
+Map_b1fd_0:	spriteHeader
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_0_End
+Map_b1fd_0_End
 
-Map_e82f_1:	spriteHeader
+Map_b1fd_1:	spriteHeader
  spritePiece -8, -$B, 2, 2, $D, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_1_End
+Map_b1fd_1_End
 
-Map_e82f_2:	spriteHeader
+Map_b1fd_2:	spriteHeader
  spritePiece -8, -$B, 2, 2, $11, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_2_End
+Map_b1fd_2_End
 
-Map_e82f_3:	spriteHeader
+Map_b1fd_3:	spriteHeader
  spritePiece -8, -$B, 2, 2, $15, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_3_End
+Map_b1fd_3_End
 
-Map_e82f_4:	spriteHeader
+Map_b1fd_4:	spriteHeader
  spritePiece -8, -$B, 2, 2, $19, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_4_End
+Map_b1fd_4_End
 
-Map_e82f_5:	spriteHeader
+Map_b1fd_5:	spriteHeader
  spritePiece -8, -$B, 2, 2, $21, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_5_End
+Map_b1fd_5_End
 
-Map_e82f_6:	spriteHeader
+Map_b1fd_6:	spriteHeader
  spritePiece -8, -$B, 2, 2, $25, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_6_End
+Map_b1fd_6_End
 
-Map_e82f_7:	spriteHeader
+Map_b1fd_7:	spriteHeader
  spritePiece -8, -$B, 2, 2, $29, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_7_End
+Map_b1fd_7_End
 
-Map_e82f_8:	spriteHeader
+Map_b1fd_8:	spriteHeader
  spritePiece -8, -$B, 2, 2, $2D, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_8_End
+Map_b1fd_8_End
 
-Map_e82f_9:	spriteHeader
+Map_b1fd_9:	spriteHeader
  spritePiece -8, -$B, 2, 2, $31, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_9_End
+Map_b1fd_9_End
 
-Map_e82f_10:	spriteHeader
+Map_b1fd_10:	spriteHeader
  spritePiece -8, -$B, 2, 2, $1D, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_10_End
+Map_b1fd_10_End
 
-Map_e82f_11:	spriteHeader
+Map_b1fd_11:	spriteHeader
  spritePiece -$10, -1, 4, 2, $35, 0, 0, 0, 0
-Map_e82f_11_End
+Map_b1fd_11_End
 
-Map_e82f_12:	spriteHeader
+Map_b1fd_12:	spriteHeader
  spritePiece -8, -$B, 2, 2, $3D, 0, 0, 0, 0
  spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
  spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
-Map_e82f_12_End
+Map_b1fd_12_End
+
+Map_b1fd_13:	spriteHeader
+ spritePiece -8, -$B, 2, 2, $41, 0, 0, 0, 0
+ spritePiece -$10, -$11, 3, 4, 0, 0, 0, 0, 0
+ spritePiece 8, -$11, 1, 3, 0, 1, 0, 0, 0
+ spritePiece 8, 7, 1, 1, $C, 0, 0, 0, 0
+Map_b1fd_13_End
 
 	even
