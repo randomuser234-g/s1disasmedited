d638d604c8d28b892af2d3e6901c17b70bbf8f73
randomuser234-g <thoratkihome@gmail.com>
2026-03-20 21:08:42 -0500

    re-label the file
    some labels were there in the porting job that I forgot to put back
    the original prototype labels are ; out

--- _incObj/TailsFlight.asm
+++ _incObj/TailsFlight.asm
@@ -12,7 +12,7 @@ Tails_Flight:
 		btst	#2,obStatus(a0)
 		beq.s	Offset_0x00E382
 		bclr	#2,obStatus(a0)
-		;move.b	Obj_Height_2(a0),d1
+		;move.b	Obj_Height_2(a0),d1			
 		;move.b	Obj_Height_3(a0),Obj_Height_2(a0)
 		;move.b	Obj_Width_3(a0),Obj_Width_2(a0)
 		;sub.b	Obj_Height_3(a0),d1
@@ -22,7 +22,7 @@ Tails_Flight:
 
 Offset_0x00E382:
 		move.b	#1,(f_doublejump).w
-		andi.b	#$30,d0
+		andi.b	#btnB|btnC,d0
 		beq.s	.20anim
 		move.b	#2,(f_doublejump).w
 
@@ -44,47 +44,53 @@ Tails_StartFlying:
 		rts
 	.flying:
 		cmpi.b	#1,(f_doublejump).w
-		bne.s	Offset_0x00DC3E
+		bne.s	FlyP1
 		move.b	(v_jpadhold2p2+1).w,d0
-		andi.b	#$30,d0		;not sure of button value $30, doesn't get mentioned in s1 constants
-		beq.s	Offset_0x00DC18
+		andi.b	#btnB|btnC,d0		;$30?
+		beq.s	Tails_Speed1
 		subi.w	#$40,obVelY(a0)
-		bra.s	Offset_0x00DC28
+		bra.s	Tails_Speed2
 
-Offset_0x00DC18:
+;Offset_0x00DC18:
+Tails_Speed1:
 		move.b	(v_jpadhold2p2).w,d0
 		andi.b	#btnA,d0		;2 flying styles, if A is held, smooth acceleration flight
-		beq.s	Offset_0x00DC28		;if A isn't held, C must be pressed and will "bob" tails up in the air in multiple spirts, similar to jump
+		beq.s	Tails_Speed2		;if A isn't held, C must be pressed and will "bob" tails up in the air in multiple spirts, similar to jump
 		subi.w	#$10,obVelY(a0)
 
-Offset_0x00DC28:
+;Offset_0x00DC28:
+Tails_Speed2:
 		addi.w	#8,obVelY(a0)
 		cmpi.w	#-$100,obVelY(a0)
-		bge.s	Offset_0x00DC3C
+		bge.s	Fly_DoNothing
 		move.w	#-$100,obVelY(a0)
 
-Offset_0x00DC3C:
+;Offset_0x00DC3C:
+Fly_DoNothing:
                 rts
 ; ---------------------------------------------------------------------------
 
-Offset_0x00DC3E:
+;Offset_0x00DC3E:
+FlyP1:
 		move.b	(v_jpadhold2p2+1).w,d0
-		andi.b	#$30,d0
-		beq.s	Offset_0x00DC56
+		andi.b	#btnB|btnC,d0
+		beq.s	FlyP2
 		tst.w	obVelY(a0)
-		bmi.s	Offset_0x00DC6C
+		bmi.s	FlyP3
 		subi.w	#$300,obVelY(a0)
-		bra.s	Offset_0x00DC6C
+		bra.s	FlyP3
 
-Offset_0x00DC56:
+;Offset_0x00DC56:
+FlyP2:
 		move.b	(v_jpadhold2p2).w,d0
 		andi.b	#btnA,d0
-		beq.s	Offset_0x00DC6C
+		beq.s	FlyP3
 		tst.w	obVelY(a0)
-		bmi.s	Offset_0x00DC6C
+		bmi.s	FlyP3
 		subi.w	#$200,obVelY(a0)
 
-Offset_0x00DC6C:
+;Offset_0x00DC6C:
+FlyP3:
 		addi.w	#8,obVelY(a0)
 		rts
 ; End of function Tails_StartFlying
