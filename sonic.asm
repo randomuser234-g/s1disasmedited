c1d644923793654a160937c22291cfe78ba1a0a8
randomuser234-g <thoratkihome@gmail.com>
2026-05-24 07:17:14 -0500

    2nd controller support
    seems it just needed this extra line in ReadJoypads to get it to map to player 2. I don't get it though, considering sonic 2 doesn't need this and supports it fine

--- sonic.asm
+++ sonic.asm
@@ -1024,6 +1024,7 @@ ReadJoypads:
 		lea	(v_jpadhold1).w,a0 ; address where joypad states are written
 		lea	(z80_port_1_data+1).l,a1	; first joypad port
 		bsr.s	.read		; do the first joypad
+		lea	(v_jpadhold1p2).w,a0 ; new line to support second joypad
 		addq.w	#2,a1		; do the second joypad
 
 .read:
