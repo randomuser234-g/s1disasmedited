dbb59c128effab571210fccb73b7f2ee119311dd
randomuser234-g <thoratkihome@gmail.com>
2026-07-28 21:05:32 -0500

    change this to singlecharacter
    bandaid, could cause sonic to teleport to the platform when tails touches it

--- _incObj/59 SLZ Elevators.asm
+++ _incObj/59 SLZ Elevators.asm
@@ -77,7 +77,9 @@ Elev_Main:	; Routine 0
 Elev_Platform:	; Routine 2
 		moveq	#0,d1
 		move.b	obActWid(a0),d1
-		jsr	(PlatformObject).l
+	lea	(v_player).w,a1
+	moveq	#p1_standing_bit,d6
+		jsr	(PlatformObject_SingleCharacter).l
 		bra.w	Elev_Types
 ; ===========================================================================
 
